24b98febd27eb137fcd6fab7ff37d8383fe3d363
KAZUMA Ukyo <yaakaito@gmail.com>
2012-02-21 11:45:03 +0900

    想いを書き連ねた

---
+++ README.mk
@@ -0,0 +1,34 @@
+# NLTHTTPStubServer
+
+## Feature
+exsample on GHUnit
+
+```objective-c
+- (void)setUpClass {
+    [NLTHTTPStubServer globalSettings].port = 12345;
+    [NLTHTTPStubServer globalSettings].shouldAutoStart = YES;
+    server =  [NLTHTTPStubServer stubServer];
+}
+
+- (void)setUp {
+    [server clear];
+}
+
+- (void)tearDown {
+    if(![server isEmpty]){
+        GHFail(@"dont call %f stubs", [server.stubs count]);
+    }
+}
+
+- (void)testHttpRequest {
+    [[[server next] andStatusCode:200] andResponse:[NLTResponse jsonResponseWithData:[NSData data]]];
+    [[[server next] andStatusCode200] andResponse:[NLTResponse jsonResponseWithData[NSData data]]];
+    [self prepare];
+    // throw 2 httprequests and parsers
+    NLFooClient *client = [NLFooClient client];
+    [client startRequestWithComplete:^{
+        [self notify:kGHUnitWaitStatusSuccess];
+    }];
+    [self waitForTimeout:kGHUnitWaitStatusSuccess];
+}
+```
