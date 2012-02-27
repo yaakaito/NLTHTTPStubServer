6cabddaa1dd2be1ae65eee5ef19220da06145fc3
KAZUMA Ukyo <yaakaito@gmail.com>
2012-02-28 05:08:21 +0900

    import GHUnit

---
+++ NLTHTTPStubServerTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NLTHTTPStubServerTests
+//
+//  Created by  on 12/02/28.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIPhoneAppDelegate");
+    }
+}
