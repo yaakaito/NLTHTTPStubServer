9b2d53f0ed4dccc1a7603f1dc70fc03ee76b1d43
KAZUMA Ukyo <yaakaito@gmail.com>
2012-02-27 11:39:56 +0900

    import xcode project

---
+++ NLTHTTPStubServer/NLTHTTPStubServer.h
@@ -0,0 +1,13 @@
+//
+//  NLTHTTPStubServer.h
+//  NLTHTTPStubServer
+//
+//  Created by KAZUMA Ukyo on 12/02/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLTHTTPStubServer : NSObject
+
+@end
---
+++ NLTHTTPStubServer/NLTHTTPStubServer.m
@@ -0,0 +1,23 @@
+//
+//  NLTHTTPStubServer.m
+//  NLTHTTPStubServer
+//
+//  Created by KAZUMA Ukyo on 12/02/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "NLTHTTPStubServer.h"
+
+@implementation NLTHTTPStubServer
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
