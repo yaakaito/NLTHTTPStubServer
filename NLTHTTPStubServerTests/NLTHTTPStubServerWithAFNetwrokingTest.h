f119c11164a0807415eb069bdd7e6a420ed9894a
KAZUMA Ukyo <yaakaito@gmail.com>
2012-04-26 23:43:02 +0900

    AFNetworkingでチェイン方式のテスト書いた

---
+++ NLTHTTPStubServerTests/NLTHTTPStubServerWithAFNetwrokingTest.h
@@ -0,0 +1,15 @@
+//
+//  NLTHTTPStubServerWithAFNetwrokingTest.h
+//  NLTHTTPStubServer
+//
+//  Created by KAZUMA Ukyo on 12/04/26.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import "NLTHTTPStubServer.h"
+
+@interface NLTHTTPStubServerWithAFNetwrokingTest : GHAsyncTestCase {
+    NLTHTTPStubServer *server;
+}
+@end
